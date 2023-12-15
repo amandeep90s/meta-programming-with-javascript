77ac75cf02011afe2de9c857335e81afa6ebd297
Amandeep Singh <AmandeepSingh@emerson.com>
2023-12-15 12:15:19 +0530

    Add arithmetic and logical operators to operators.js

---
+++ operators.js
@@ -0,0 +1,23 @@
+/**
+  Arithmetic operators
+  + addition
+  - subtraction
+  / division
+  * multiplication
+ */
+
+/**
+  Logical operators
+  && and
+  || or
+  ! not
+ */
+
+console.log(2 + 2);
+console.log(3 - 1);
+console.log(4 / 2);
+console.log(3 * 2);
+console.log(5 > 10 && 10 > 5);
+console.log(5 > 10 || 10 > 5);
+console.log(4 == 4);
+console.log(!(5 != 6));
