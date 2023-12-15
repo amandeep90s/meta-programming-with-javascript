4ae914eb143fbd0e4c6b6833563d59912273816d
Amandeep Singh <AmandeepSingh@emerson.com>
2023-12-15 12:11:29 +0530

    Add console.log statements and comments

---
+++ comments.js
@@ -0,0 +1,9 @@
+// This is single line comment
+
+/*
+This is a
+multi line
+comment
+*/
+
+/* This is a multi line comment on a single line */
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("Hello World!");
 console.log("My name is Amandeep Singh");
+console.log(
+  "%cAmandeep Singh",
+  "color: red; font-size: 30px; font-weight: bold;"
+);
+console.log("Hello " + "there, " + "world");
