819bf84e3b46058770be372cea5c80c7debb90b3
Amandeep Singh <AmandeepSingh@emerson.com>
2023-12-16 20:34:10 +0530

    Add loops.js file with for, while, and nested loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+// for loop
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// while loop
+var counter = 3;
+while (counter > 0) {
+  console.log(counter);
+  counter--;
+}
+// nested loop
+for (let i = 2023; i < 2025; i++) {
+  for (let j = 6; j < 9; j++) {
+    console.log(i + " " + j);
+  }
+}
